237e8cf07dc05c2268d83152d4588a0279c8cacc
Neil Lawrence <N.Lawrence@sheffield.ac.uk>
2016-04-28 07:19:30 -0700

    Add amazon talk.

---
+++ slides/demos/demo_2016_04_28_amazon.m
@@ -0,0 +1,25 @@
+% Demo for OxWaSP seminar 2016
+% Code needed at:de
+% https://github.com/SheffieldML/multigp
+% https://github.com/SheffieldML/hgplvm
+% https://github.com/SheffieldML/hsvargplvm
+% https://github.com/SheffieldML/GPmat
+% and dependencies
+
+importTool('multigp')
+multigpToolboxes
+clear all, close all
+demAistats
+pause
+
+importTool('hgplvm')
+hgplvmToolboxes
+clear all, close all
+load demWalkRun1
+hgplvmHierarchicalVisualise(model, visualiseNodes, depVisData)
+pause
+
+importTool('hsvargplvm')
+hsvargplvmToolboxes
+clear all, close all
+demDigitsDemonstration
