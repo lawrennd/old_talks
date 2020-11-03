a3b2a8773f522705165d2390d8b38a9cc4f4d805
Neil Lawrence <lawrennd@gmail.com>
2020-11-03 08:40:19 +0000

    Update for new figure-animate.js from proceedings.

--- make-slides.mk
+++ make-slides.mk
@@ -1,11 +1,11 @@
 ${BASE}.slides.html: ${BASE}.slides.html.markdown ${BIBDEPS}
-	printf '' > ../include.tmp
-	printf '\[' >> ../include.tmp
-	cat ../_includes/talk-notation.tex >> ../include.tmp
-	printf '\]' >> ../include.tmp
-	printf '' >> ../include.tmp
+	#printf '' > ../include.tmp
+	#printf '\[' >> ../include.tmp
+	#cat ../_includes/talk-notation.tex >> ../include.tmp
+	#printf '\]' >> ../include.tmp
+	#printf '' >> ../include.tmp
 	#pandoc  --template pandoc-revealjs-template ${PDSFLAGS} ${SFLAGS} -c ${CSS} --include-in-header=${SLIDESHEADER} -t revealjs --bibliography=../lawrence.bib --bibliography=../other.bib --bibliography=../zbooks.bib -o ${BASE}.slides.html  ${BASE}.slides.html.markdown 
-	pandoc  -B ../include.tmp --template pandoc-revealjs-template ${PDSFLAGS} ${SFLAGS} -c ${CSS} --include-in-header=${SLIDESHEADER} -t revealjs --bibliography=../lawrence.bib --bibliography=../other.bib --bibliography=../zbooks.bib -o ${BASE}.slides.html  ${BASE}.slides.html.markdown 
+	pandoc --template pandoc-revealjs-template ${PDSFLAGS} ${SFLAGS} -c ${CSS} --include-in-header=${SLIDESHEADER} -t revealjs --bibliography=../lawrence.bib --bibliography=../other.bib --bibliography=../zbooks.bib -o ${BASE}.slides.html  ${BASE}.slides.html.markdown 
 	cp ${BASE}.slides.html ${SLIDEDIR}/${OUT}.slides.html
 	../copy_web_diagrams.sh ${BASE}.md
 	rm ../include.tmp
