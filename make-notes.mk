dc88de52e1680211ffbd94745bcf528a5d9719cf
Neil Lawrence <lawrennd@gmail.com>
2021-02-14 19:06:45 +0000

    Update with Nic's diagrams and slides

--- make-notes.mk
+++ make-notes.mk
@@ -2,15 +2,9 @@
 	${PP} $< -o $@ --format notes --to html --code sparse --replace-notation --edit-links --exercises ${PPFLAGS} 
 
 %.notes.docx.markdown: %.md ${DEPS}
-	${PP} $< -o $@ --format notes --to docx --code sparse --replace-notation --edit-links ${PPFLAGS} 
+	${PP} $< -o $@ --format notes --to docx --code sparse --replace-notation --edit-links ${PPFLAGS}
+	echo ${BASE}
 
-${BASE}.docx: ${BASE}.notes.docx.markdown ${BIBDEPS} ${DIAGDEPS}
-	pandoc  ${CITEFLAGS} \
-		--to docx \
-		-B ../_includes/${NOTATION} \
-		${DOCXFLAGS} \
-		--out ${BASE}.docx  \
-		${BASE}.notes.docx.markdown 
 
 ${BASE}.notes.html: ${BASE}.notes.html.markdown ${BIBDEPS}
 	pandoc  ${PDSFLAGS} \
