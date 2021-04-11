2819557aa2986bb8df49817cbb9f3d593b3e40ce
Neil Lawrence <lawrennd@gmail.com>
2021-04-11 17:52:42 +0100

    Updates after moving maketalk python code out.

--- copy_web_diagrams.sh
+++ copy_web_diagrams.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-SLIDEDIR=$(../talkfield.py slidedir $1)
+SLIDEDIR=$(talkfield slidedir $1)
 echo Slides Directory: $SLIDEDIR
-for file in $(../dependencies.py slidediagrams $1)
+for file in $(dependencies slidediagrams $1)
 do
     mkdir -p `dirname $SLIDEDIR/$file`
     if ! cmp -s $file $SLIDEDIR/$file ; then
