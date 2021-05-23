19f81c8dad9594212af4acb1162aefb78138c739
Neil Lawrence <lawrennd@gmail.com>
2021-05-23 19:55:06 +0100

    Update with talks.

--- make-slides.mk
+++ make-slides.mk
@@ -8,7 +8,6 @@
 ${BASE}.slides.html: ${BASE}.slides.html.markdown ${BIBDEPS}
 	pandoc --template pandoc-revealjs-template ${PDSFLAGS} ${SFLAGS} --include-in-header=../_includes/${SLIDESHEADER} -t revealjs ${BIBFLAGS} -o ${BASE}.slides.html  ${BASE}.slides.html.markdown 
 	cp ${BASE}.slides.html ${SLIDEDIR}/${OUT}.slides.html
-	../copy_web_diagrams.sh ${BASE}.md
 
 ${BASE}.pptx: ${BASE}.slides.pptx.markdown 
 	pandoc  -t pptx \
