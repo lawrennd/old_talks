d34f90616b43ef63b43a8a491d3e38b96d6b671d
Neil Lawrence <lawrennd@gmail.com>
2021-10-22 08:35:19 +0100

    Move some material from simulation to emulation according to where we got to on Tuesday.

--- make-talk-flags.mk
+++ make-talk-flags.mk
@@ -29,7 +29,7 @@ CITEFLAGS=--citeproc --csl=../elsevier-harvard.csl ${BIBFLAGS}
 PDSFLAGS=-s ${CITEFLAGS} --mathjax=${MATHJAX} 
 
 DIAGRAMSDIR=$(shell mdfield diagramsdir $(BASE).md)
-WRITEDIAGRAMSDIR=$(shell mdfield diagramsdir $(BASE).md)
+WRITEDIAGRAMSDIR=$(shell mdfield writediagramsdir $(BASE).md)
 POSTSDIR=$(shell mdfield postsdir $(BASE).md)
 NOTESDIR=$(shell mdfield notesdir $(BASE).md)
 NOTEBOOKSDIR=$(shell mdfield notebooksdir $(BASE).md)
